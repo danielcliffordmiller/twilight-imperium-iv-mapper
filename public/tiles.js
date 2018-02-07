c254aef210801f25984103257d0b0b48f30ef5c7
Dan Miller <Daniel.C.Miller@target.com>
2018-02-07 08:35:07 -0600

    removed unnecessary code

--- public/tiles.js
+++ public/tiles.js
@@ -1,18 +1,10 @@
 
 "use strict";
 
-var selected;
-var removePreviousSelected = function() {
-    removePreviousSelected = function() {
-	selected.removeClass("selected")
-    }
-}
 $(document).ready(function() {
     $(".hand").click(function(event) {
-	removePreviousSelected()
-	$(".hand").removeClass("selected");
-	selected = $("."+this.id);
-	selected.addClass("selected");
+	$(".selected").removeClass("selected");
+	$("."+this.id).addClass("selected");
 	$(this).addClass("selected");
     });
 });
