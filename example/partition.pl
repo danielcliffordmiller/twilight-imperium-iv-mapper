5441ff6fbca277ad10ca7ac5bb19186c37915269
Dan Miller <Daniel.C.Miller@target.com>
2018-02-09 09:47:32 -0600

    pass a named function

--- example/partition.pl
+++ example/partition.pl
@@ -8,10 +8,18 @@ use Utils;
 
 use v5.12;
 
+sub is_this_number_even {
+    return not ($_[0] % 2);
+}
+
+say "2 is even" if is_this_number_even(2);
+say "1 is not"	unless is_this_number_even(1);
+say;
+
 my ($list_of_3, $list_of_even, $list_of_leftovers) =
     partition(
 	sub { $_[0] == 3 }, # <- this function evaluates to true if its argument == 3
-	sub { not ($_[0] %2) }, # <- this function evals to true if its argument is even
+	\&is_this_number_even, # <- this function evals to true if its argument is even
 	(1,2,3,4,5) # <- this is the list of whatever that we are partitioning
     );
 
