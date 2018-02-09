e13296ca79c7f7002c19e98bd2fb3b876268f12b
Dan Miller <Daniel.C.Miller@target.com>
2018-02-08 23:55:21 -0600

    added partition function

---
+++ lib/Utils.pm
@@ -0,0 +1,35 @@
+package Utils;
+
+use strict;
+
+use v5.12;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our @EXPORT = qw(partition);
+
+sub partition {
+    my @subs;
+
+    push @subs, shift until ref $_[0] ne 'CODE';
+
+    my @res = ( map { [] } @subs, [] );
+
+    item: foreach my $item (@_) {
+	for (my $i = 0; $i < scalar(@subs); $i++) {
+	    if ( $subs[$i]($item) ) {
+	        push @{ $res[$i] }, $item;
+	        next item;
+	    }
+	    #push @{ $res[$i] }, $item and next item if $subs[$i]($item);
+	    # ^^ does the same thing but is less clear
+	}
+	push @{ $res[-1] }, $item;
+    }
+
+    return @res;
+}
+
+1;
