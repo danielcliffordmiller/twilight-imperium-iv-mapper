4753fcb2eb7e51730d57f30e49d62ede71200492
Dan Miller <Daniel.C.Miller@target.com>
2018-03-01 20:17:32 -0600

    players rendered as links

--- lib/SVG.pm
+++ lib/SVG.pm
@@ -9,18 +9,22 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-our @EXPORT_OK = qw(active_players);
+our @EXPORT_OK = qw(render_active_players);
 
 use HTML::Element;
 use Mojo::ByteStream qw(b);
 
-sub active_players {
+sub render_active_players {
     my $s = shift;
     my ($players, $active) = ($s->players(), $s->active_player);
 
-    return b(HTML::Element->new_from_lol( [ 'ul', map {
-		[ 'li', $_ == $active ? { class => 'active' } : (), $_->[1] ]
-	    } @$players ] )->as_HTML());
+    return html([ 'ul', map {
+	[ 'li', $_ == $active ? { class => 'active' } : (), [ 'a', {href => "/".(join '/', 's', $s->id(), $_->[0])}, $_->[1] ] ]
+    } @$players ]);
+}
+
+sub html {
+    return b(HTML::Element->new_from_lol( @_ )->as_HTML());
 }
 
 1;
