eeeea43b73559c9301fd7ef365213d93147114d5
Dan Miller <Daniel.C.Miller@target.com>
2018-03-01 21:22:11 -0600

    added outline function, removed outline template

--- lib/SVG.pm
+++ lib/SVG.pm
@@ -9,7 +9,7 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-our @EXPORT_OK = qw(render_active_players);
+our @EXPORT_OK = qw(render_active_players outline);
 
 use HTML::Element;
 use Mojo::ByteStream qw(b);
@@ -21,6 +21,18 @@ sub render_active_players {
     return html([ 'ul', map {
 	[ 'li', $_ == $active ? { class => 'active' } : (), [ 'a', {href => "/".(join '/', 's', $s->id(), $_->[0])}, $_->[1] ] ]
     } @$players ]);
+}
+
+sub outline {
+    my $params = shift || {};
+    return html( [ 'path', {
+		%$params,
+		d	=> "M -25 -43 l 50 0 25 43 -25 43 -50 0 -25 -43 z",
+		fill	=> 'black',
+		'fill-opacity'	=> '0.0',
+		'stroke-width'	=> '2',
+		stroke	=> 'black'
+	    } ] );
 }
 
 sub html {
