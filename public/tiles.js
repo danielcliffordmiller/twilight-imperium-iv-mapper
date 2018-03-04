fc3d8fbfd815063272d70cf1d142bdc335c63717
Dan Miller <danielcliffordmiller@gmail.com>
2018-03-03 20:46:16 -0600

    html elements come with click handlers

--- public/tiles.js
+++ public/tiles.js
@@ -1,29 +1,28 @@
 
 "use strict";
 
+function handleHandClick() {
+    $(".selected").removeClass("selected");
+    $("."+this.getAttribute("tile-type")).addClass("selected");
+    $(this).addClass("selected");
+}
+function handleMapClick() {
+    const elt = this;
+    if ($(this).hasClass("selected")) {
+	$("#confirm").addClass("active");
+	$('#c-yes').off("click");
+	$("#c-yes").click(function(event){
+	    $("#confirm").removeClass("active");
+	    Intercooler.triggerRequest(elt);
+	});
+    }
+}
 function handleReady() {
-    $(".hand").click(function(event) {
-	$(".selected").removeClass("selected");
-	$("."+this.getAttribute("tile-type")).addClass("selected");
-	$(this).addClass("selected");
-    });
-    $(".map").click(function(event) {
-        const elt = this;
-        if ($(this).hasClass("selected")) {
-            $("#confirm").addClass("active");
-            $('#c-yes').off("click");
-            $("#c-yes").click(function(event){
-                $("#confirm").removeClass("active");
-                Intercooler.triggerRequest(elt);
-            });
-        }
-    });
     $(document).on("beforeAjaxSend.ic", function(event, ajaxSetup, elt) {
 	ajaxSetup.data = ajaxSetup.data + "&hand=" + $(".hand.selected")[0].id;
     });
     $("#c-no").click(function(event){
-        $("#confirm").removeClass("active");
+	$("#confirm").removeClass("active");
     });
 }
 $(document).ready(handleReady);
-Intercooler.ready(function(elt){ handleReady() });
