9ada416636531c163bc71d397ae68908a8341af8
Dan Miller <danielcliffordmiller@gmail.com>
2018-06-09 12:42:56 -0500

    fixed whitespace

--- public/tiles.js
+++ public/tiles.js
@@ -2,27 +2,27 @@
 "use strict";
 
 function handleHandClick() {
-    $(".selected").removeClass("selected");
-    $("."+this.getAttribute("tile-type")).addClass("selected");
-    $(this).addClass("selected");
+  $(".selected").removeClass("selected");
+  $("."+this.getAttribute("tile-type")).addClass("selected");
+  $(this).addClass("selected");
 }
 function handleMapClick() {
-    const elt = this;
-    if ($(this).hasClass("selected")) {
-	$("#confirm").addClass("active");
-	$('#c-yes').off("click");
-	$("#c-yes").click(function(event){
+  const elt = this;
+  if ($(this).hasClass("selected")) {
+    $("#confirm").addClass("active");
+    $('#c-yes').off("click");
+    $("#c-yes").click(function(event){
 	    $("#confirm").removeClass("active");
 	    Intercooler.triggerRequest(elt);
-	});
-    }
+    });
+  }
 }
 function handleReady() {
-    $(document).on("beforeAjaxSend.ic", function(event, ajaxSetup, elt) {
-	ajaxSetup.data = ajaxSetup.data + "&hand=" + $(".hand.selected")[0].id;
-    });
-    $("#c-no").click(function(event){
-	$("#confirm").removeClass("active");
-    });
+  $(document).on("beforeAjaxSend.ic", function(event, ajaxSetup, elt) {
+    ajaxSetup.data = ajaxSetup.data + "&hand=" + $(".hand.selected")[0].id;
+  });
+  $("#c-no").click(function(event){
+    $("#confirm").removeClass("active");
+  });
 }
 $(document).ready(handleReady);
