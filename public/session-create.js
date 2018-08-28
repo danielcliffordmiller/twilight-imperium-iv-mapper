ba76773dd181ef95790f51f0002df2e9b6a37817
Dan Miller <danielcliffordmiller@gmail.com>
2018-08-27 20:33:02 -0500

    fire on actual text entry

--- public/session-create.js
+++ public/session-create.js
@@ -1,10 +1,11 @@
 "use strict";
 $(document).ready(function(){
   $("input[type=text]").keyup(function(e){
-    if(/^\s*$/.test(e.target.value)) {
-      e.target.labels.forEach( (l) => $(l).addClass("empty") );
+    if(/^\s*$/.test(this.value)) {
+      $("label[for="+this.id+"]").addClass("empty");
     } else {
-      e.target.labels.forEach( (l) => $(l).removeClass("empty") );
+      this.dispatchEvent(new Event('data'));
+      $("label[for="+this.id+"]").removeClass("empty");
     }
   });
 });
